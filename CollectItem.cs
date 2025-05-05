fa2f4891cc58126621760483e933421115e8cea5
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2025-05-05 18:25:01 +0900

    It works

--- CollectItem.cs
+++ CollectItem.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+using System.Configuration;
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using UnityEngine;
 
-namespace MyMod;
+namespace CollectItem;
 
 internal static class ModInfo
 {
@@ -16,5 +20,55 @@ internal class CollectItem : BaseUnityPlugin
     private void Awake()
     {
         Logger.LogInfo("CollectItem Mod is loaded!");
+        Settings.keyCode = Config.Bind("Settings", "KeyCode", KeyCode.C, new ConfigDescription("Key to collect items", null, null));
+        Settings.keyCodeMod = Config.Bind("Settings", "KeyCodeMod", KeyCode.LeftAlt, new ConfigDescription("Modifier key to collect item. If None is specified, it does not require a modifier key.", null, null));
+    }
+
+    private void Update()
+    {
+        if (!EClass.core.IsGameStarted)
+        {
+            return;
+        }
+
+        if (!EClass._zone.IsPCFaction)
+        {
+            return;
+        }
+        
+
+        if ((Settings.KeyCodeMod == KeyCode.None || Input.GetKey(Settings.KeyCodeMod)) && Input.GetKeyDown(Settings.KeyCode))
+        {
+            Logger.LogInfo("CollectItem: Key pressed. Starting collection.");
+
+            Point.map.ForeachPoint(point =>
+            {
+                DoCollectItem(point);
+            });
+        }
+    }
+
+    // This method is based on Perform method in Elin/AM_Deconstruct.cs.
+    private void DoCollectItem(Point point)
+    {
+        List<Card> list = point.ListCards();
+		list.Reverse();
+		foreach (Card item in list.Copy())
+		{
+			if ((!EClass.debug.ignoreBuildRule && (!item.isThing || !item.trait.CanPutAway)) || item.IsInstalled || item.IsPCParty)
+			{
+				continue;
+			}
+
+            item.PlaySound(item.material.GetSoundDead(item.sourceCard));
+            if (item.isThing)
+            {
+                EClass._map.PutAway(item.Thing);
+            }
+            else
+            {
+                item.Destroy();
+            }
+		}
     }
 }
---
+++ Settings.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace CollectItem;
+
+public static class Settings
+{
+    public static ConfigEntry<KeyCode> keyCode;
+    public static ConfigEntry<KeyCode> keyCodeMod;
+
+    public static KeyCode KeyCode
+    {
+        get { return keyCode.Value; }
+        set { keyCode.Value = value; }
+    }
+
+    public static KeyCode KeyCodeMod
+    {
+        get { return keyCodeMod.Value; }
+        set { keyCodeMod.Value = value; }
+    }
+}
